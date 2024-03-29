06756e9c107e6dd1af6223e36debf95fbc92d4d1
Aarthi20031993 <aaarthianu@gmail.com>
2024-03-29 22:33:39 +0530

    work done

---
+++ src/org/github/GitSample.java
@@ -0,0 +1,16 @@
+package org.github;
+
+public class GitSample {
+	
+public static void main()   {
+		
+		System.out.println("Working directory");
+		System.out.println("Stagging area");
+		System.out.println("Local git repository");
+		System.out.println("Remote git repository");
+	
+	
+	}
+
+
+}
