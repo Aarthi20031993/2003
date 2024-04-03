31e575258a74360d4859f6b476436edf6fc8fbdf
Aarthi20031993 <aaarthianu@gmail.com>
2024-04-03 23:10:57 +0530

    Work done

--- src/org/github/GitSample.java
+++ src/org/github/GitSample.java
@@ -8,7 +8,19 @@ public static void main()   {
 		System.out.println("Stagging area");
 		System.out.println("Local git repository");
 		System.out.println("Remote git repository");
+		
+		String s="Java Programme";
+		
+		String op="";
+		
+		for(int i=s.length()-1;i>=0;i--)  {
+			
+			char c = s.charAt(i);
+			op=op+c;
+			
+		}
 	
+		System.out.println(op);
 	
 	}
 
