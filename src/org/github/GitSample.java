689b511ad5e94114a56927f621c895b69a4d6e8b
Aarthi20031993 <aaarthianu@gmail.com>
2024-04-04 22:18:58 +0530

    Work done B

--- src/org/github/GitSample.java
+++ src/org/github/GitSample.java
@@ -1,5 +1,8 @@
 package org.github;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GitSample {
 	
 public static void main()   {
@@ -8,6 +11,19 @@ public static void main()   {
 		System.out.println("Stagging area");
 		System.out.println("Local git repository");
 		System.out.println("Remote git repository");
+		
+		List<String> li=new ArrayList<String>();
+		
+		li.add("A");
+		li.add("B");
+		li.add("C");
+		li.add("D");
+		li.add("E");
+		
+		for(int i=0;i<li.size();i++)  {
+			
+			System.out.println(li.get(i));
+		}
 	
 	
 	}
